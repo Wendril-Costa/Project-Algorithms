f8c63d78222e9fc795dc592156cb2bcd14219d63
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-18 22:55:24 -0300

    Requisito 1 Feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        target_time is None
+        or not permanence_period
+        or not all(
+            isinstance(start, int) and isinstance(end, int) and start <= end
+            for start, end in permanence_period
+        )
+    ):
+        return None
+
+    count = sum(
+        1 for start, end in permanence_period if start <= target_time <= end
+    )
+    return count
