ebc84aaa710a117d6328868ee6a0d48519cda285
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-19 03:28:20 -0300

    Requisito 5 feito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str) or len(nums) < 2:
+        return False
+
+    nums_set = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        if num in nums_set:
+            return num
+        nums_set.add(num)
+
+    return False
