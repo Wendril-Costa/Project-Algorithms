9e617b388d28c77dced2a46914e7b27b04a10e71
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-18 23:32:33 -0300

    Requisito 2 Feito

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('trybe', 3) == 'yrt_eb'
+    assert encrypt_message('trybe', 2) == 'eby_rt'
+    with pytest.raises(TypeError):
+        encrypt_message(1, 'trybe')
+    assert encrypt_message('trybe', 6) == 'ebyrt'
+    assert encrypt_message("acaba", -1) == "abaca"
+    assert encrypt_message("dideus", 2) == "sued_id"
